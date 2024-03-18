dced5235f4c5d34b036c9115c26f32870f5d178b
Andrey Lebedev <alebedev424@gmail.com>
2024-03-18 10:17:27 +0300

    refactor: рефакторинг HighlightedText

--- src/components/HighlightedText/HighlightedText.tsx
+++ src/components/HighlightedText/HighlightedText.tsx
@@ -10,23 +10,23 @@ export function HighlightedText({ search, text }: HighlightedTextPropsType) {
   if (!search) return <>{text}</>;
   const regExp = new RegExp(`${search}`, 'ig');
   const matchValues = text.match(regExp);
-  if (matchValues) {
-    return (
-      <>
-        {text.split(regExp).map((str, index, array) => {
-          if (index < array.length - 1) {
-            const matchStr = matchValues.shift();
-            return (
-              <>
-                {str}
-                <span className={styles.highlighted}>{matchStr}</span>
-              </>
-            );
-          }
-          return <>{str}</>;
-        })}
-      </>
-    );
+  if (!matchValues) {
+    return <>{text}</>;
   }
-  return <>{text}</>;
+  return (
+    <>
+      {text.split(regExp).map((str, index, array) => {
+        if (index > array.length) {
+          return <>{str}</>;
+        }
+        const matchStr = matchValues.shift();
+        return (
+          <>
+            {str}
+            <span className={styles.highlighted}>{matchStr}</span>
+          </>
+        );
+      })}
+    </>
+  );
 }
